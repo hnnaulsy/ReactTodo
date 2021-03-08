58d7f9b8aa0acfa645e34eb0df9e9edcd8defdd8
icoderYY <icoderYY@163.com>
2021-03-08 16:29:43 +0800

    拆分组件

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react'
-import FormComponent from './Form'
+import StudentTitle from './Components/StudentTitle'
+import AddStudent from './Components/AddStudent'
+import StudentList from './Components/StudentList'
 
 class App extends Component {
 
 
   render() {
     return (
-      <div>
-        <FormComponent />
+      <div className={'container'}>
+        <StudentTitle />
+        <AddStudent />
+        <StudentList />
       </div>
     )
   }
