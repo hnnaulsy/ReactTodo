2061a33acb553ce7dff925e9ce99fd0e70d794c8
icoderYY <icoderYY@163.com>
2021-03-08 14:26:18 +0800

    受控表单之复选框

--- src/App.js
+++ src/App.js
@@ -4,21 +4,66 @@ class App extends Component {
 
   // 定义状态
   state = {
-    subject: 'java'
+    name: 'zce',
+    age: 40
+  }
+
+  // 模拟数据源  [1, 2]
+  hobbies = [
+    {
+      id: 1,
+      title: 'vue',
+      isChecked: true
+    },
+    {
+      id: 2,
+      title: 'React',
+      isChecked: false
+    },
+    {
+      id: 3,
+      title: 'Angular',
+      isChecked: false
+    },
+  ]
+
+  handler(index, ev) {
+    // console.log(ev.target.checked)
+    // console.log(index)
+    this.hobbies[index].isChecked = ev.target.checked
+  }
+
+  submit = (ev) => {
+    ev.preventDefault()
+    // 01 之前我们已经将数据与 hobbies 进行了绑定也可以同步更新 
+    // 02 当前我们想要实现的就是点击提交按钮的时候可以拿到 [1, 2]
+    let ret = this.hobbies
+      .filter(hobby => hobby.isChecked)
+      .map(hobby => hobby.id)
+
+    ret = {
+      ...this.state, ret
+    }
+
+    console.log(ret)
   }
 
   render() {
     return (
-      <div>
-        <select value={this.state.subject} onChange={(ev) => { this.setState({ subject: ev.target.value }) }}>
-          <option value="大前端">大前端</option>
-          <option value="java">java</option>
-          <option value="python">python</option>
-        </select>
-
+      <form onSubmit={(ev) => { this.submit(ev) }}>
+        {
+          this.hobbies.map((hobby, index) => {
+            return (
+              <label key={hobby.id}>
+                <input type="checkbox" defaultChecked={hobby.isChecked} onChange={this.handler.bind(this, index)} /> {hobby.title}
+              </label>
+            )
+          })
+        }
         <hr />
-        <button onClick={() => { console.log(this.state) }}>点击获取状态值</button>
-      </div>
+        <button onClick={() => { console.log(this.hobbies) }}>点击获取数据</button>
+        <input type="submit" />
+      </form>
     )
   }
 }
