072aaa5514fe2a674e60f90236690c88691a0207
icoderYY <icoderYY@163.com>
2021-03-10 09:54:17 +0800

    react发送表单请求

--- src/App.js
+++ src/App.js
@@ -1,38 +1,28 @@
 import React, { Component } from 'react'
-import About from './About'
-
-/**
- * 卸载组件 
- *  将组件从 DOM 上删除  
- * 
- * 常用方法
- *  componentWillUnMount 
- */
+import axios from 'axios'
 
 class App extends Component {
+
   constructor() {
     super()
     this.state = {
-      count: 0,
-      isShow: true
+      msg: ''
     }
-    this.handler = this.handler.bind(this)
   }
-  handler() {
-    this.setState({
-      count: this.state.count + 1
-    })
-  }
+
   render() {
     return (
       <div>
-        {this.state.count}
-        <button onClick={this.handler}>点击</button>
-        <button onClick={() => { this.setState({ isShow: !this.state.isShow }) }}>切换</button>
-        {this.state.isShow && <About />}
+        当前数据为：{this.state.msg}
       </div>
     )
   }
+
+  async componentDidMount() {
+    const data = await axios.get("http://localhost:3005/api/welcome").then(res => res.data)
+    this.setState(data)
+  }
+
 }
 
 export default App
