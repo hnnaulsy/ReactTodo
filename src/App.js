769824530031e1fc8c5cfc0b4ef68f7c93e9ffe7
icoderYY <icoderYY@163.com>
2021-03-11 14:26:04 +0800

    创建store及reducer

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react'
+import Counter from './Components/Counter'
 
 /**
- * 01 为什么需要  Redux 
- * 02 Redux是什么
- * 
- *  Redux 是一个数据管理框架， 它提供了一个叫 store 的统一数据存储的仓库
- *  store 就像是 一个数据管理的 中间人，让我们的组件之间无须再直接进行数据传递
+ * redux react-redux 
  */
 
 class App extends Component {
 
   render() {
     return (
-      <div>Redux工作流程</div>
+      <div>
+        <Counter />
+      </div>
     )
   }
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App'
+import { createStore } from 'redux'
+import CounterReducer from './Store/Reducer/Counter.reducer'
+
+const store = createStore(CounterReducer)
+
 
 ReactDOM.render(
   <React.StrictMode>
