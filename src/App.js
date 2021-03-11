8b28eadae88824292dede90aa76e4df6b5e5a056
icoderYY <icoderYY@163.com>
2021-03-11 10:27:11 +0800

    redux 工作流程

--- src/App.js
+++ src/App.js
@@ -1,40 +1,18 @@
 import React, { Component } from 'react'
-import axios from 'axios'
 
 /**
- * 01 axios 
- * 02 请求转发
- * 03 mock 数据
+ * 01 为什么需要  Redux 
+ * 02 Redux是什么
+ * 
+ *  Redux 是一个数据管理框架， 它提供了一个叫 store 的统一数据存储的仓库
+ *  store 就像是 一个数据管理的 中间人，让我们的组件之间无须再直接进行数据传递
  */
 
 class App extends Component {
 
-  constructor() {
-    super()
-    this.state = {
-      articles: []
-    }
-  }
-
-  getArticle = async () => {
-    let articles = await axios.get('/api/article.json').then(response => response.data)
-    this.setState({ articles }, () => {
-      console.log(this.state.articles)
-    })
-  }
-
   render() {
     return (
-      <div>
-        <button onClick={this.getArticle}>获取文章列表</button>
-        <ul>
-          {
-            this.state.articles.map(article => (
-              <li key={article.id}>{article.title}</li>
-            ))
-          }
-        </ul>
-      </div>
+      <div>Redux工作流程</div>
     )
   }
 
