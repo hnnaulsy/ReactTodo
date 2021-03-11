31030e6e7536e56b5f381945364cff3fbdbebaf1
icoderYY <icoderYY@163.com>
2021-03-11 16:34:57 +0800

    抽离action代码为函数

--- src/Components/Counter.js
+++ src/Components/Counter.js
@@ -5,10 +5,10 @@ function Counter(props) {
   console.log(props)
   return (
     <div>
-      <button onClick={() => { props.dispatch({ type: 'increment' }) }}>+1</button>
-      <button onClick={() => { props.dispatch({ type: 'increment_n', payload: 5 }) }}>+5</button>
+      <button onClick={props.increment}>+1</button>
+      <button onClick={() => { props.increment_n(5) }}>+5</button>
       <span>{props.count}</span>
-      <button onClick={() => { props.dispatch({ type: 'decrement' }) }}>-1</button>
+      <button onClick={props.decrement}>-1</button>
     </div>
   )
 }
@@ -18,4 +18,16 @@ const mapStateToProps = state => ({
   count: state.count
 })
 
-export default connect(mapStateToProps)(Counter)
+const mapDispatchToProps = dispatch => ({
+  increment() {
+    dispatch({ type: 'increment' })
+  },
+  increment_n(payload) {
+    dispatch({ type: 'increment_n', payload })
+  },
+  decrement() {
+    dispatch({ type: 'decrement' })
+  }
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter)
