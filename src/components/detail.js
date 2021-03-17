14f2d09a7cc46eb937a89dbc98ff3fbbfe4ebc48
icoderYY <icoderYY@163.com>
2021-03-17 19:38:45 +0800

    非路由组件传递路由信息

--- src/components/detail.js
+++ src/components/detail.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react'
 import { createHashHistory } from 'history'
+import Test from './test'
 
 
 class Detail extends Component {
-  render(props) {
-    console.log(this.props.match.params.id)
+  render() {
     return (
       <div>
         <p>{this.props.match.params.id}</p>
         <button onClick={() => { createHashHistory().push('/home') }}>点击跳转至首页</button>
+        <Test />
       </div>
     )
   }
