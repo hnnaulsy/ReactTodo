46ee13fd9ff77f54ee112429e41e75d997e542a4
icoderYY <icoderYY@163.com>
2021-03-19 15:43:13 +0800

    useContext 使用

--- src/App.js
+++ src/App.js
@@ -1,53 +1,51 @@
-import React, { useState, useEffect } from 'react'
+import React, { createContext, useContext } from 'react'
 
+const UserContext = createContext({})
+const AreaContext = createContext({})
 
-/**
- */
-
+// 定义 Header 组件
 function Header() {
-  const [strState, setStrState] = useState('拉勾教育')
-  const [numState, setNumState] = useState(10)
-
-  useEffect(() => {
-    // 事件监听添加操作
-    console.log('事件监听添加操作')
-    return () => {
-      console.log('移除事件监听')
-    }
-  })
-
-  useEffect(() => {
-    // 发送网络请求获取数据
-    console.log('发送网络请求获取数据')
-  })
-
-  useEffect(() => {
-    // 判断是否登录
-    console.log('判断是否登录')
-  })
-
-  useEffect(() => {
-    console.log('挂载与更新操作完成了')
-    return () => {
-      console.log('卸载的时候执行')
-    }
-  }, [strState])
-
+  const userInfo = useContext(UserContext)
+  const areaInfo = useContext(AreaContext)
   return (
+    // <UserContext.Consumer>
+    //   {
+    //     value1 => {
+    //       return (
+    //         <AreaContext.Consumer>
+    //           {
+    //             value2 => {
+    //               return (
+    //                 <div>
+    //                   <p>{value1.name}</p>
+    //                   <p>{value1.age}</p>
+    //                   <p>{value2.width}</p>
+    //                 </div>
+    //               )
+    //             }
+    //           }
+    //         </AreaContext.Consumer>
+    //       )
+    //     }
+    //   }
+    // </UserContext.Consumer>
+
     <div>
-      <p>{strState} <button onClick={() => { setStrState('大前端') }}>修改字符</button></p>
-      <p>{numState} <button onClick={() => { setNumState(numState + 1) }}>修改数值</button></p>
+      <p>{userInfo.name}</p>
+      <p>{userInfo.age}</p>
+      <p>{areaInfo.width}</p>
     </div>
   )
 }
 
 function App() {
-  const [isShow, setIsShow] = useState(true)
   return (
     <div>
-      {isShow && <Header />}
-      <hr />
-      <button onClick={() => { setIsShow(!isShow) }}>切换</button>
+      <UserContext.Provider value={{ "name": "syy", "age": 18 }}>
+        <AreaContext.Provider value={{ "width": 200 }}>
+          <Header />
+        </AreaContext.Provider>
+      </UserContext.Provider>
     </div>
   )
 }
