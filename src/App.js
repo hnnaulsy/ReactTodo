91582a8abdf0cfd1c72ee2ec6d5164065fb4c2b8
icoderYY <icoderYY@163.com>
2021-03-19 10:22:25 +0800

    useState设置状态

--- src/App.js
+++ src/App.js
@@ -1,37 +1,36 @@
-import React, { Component } from 'react'
-import { Link, Route, Switch } from 'react-router-dom'
-import AuthRouteGuard from './guard'
-import loadComponent from '@loadable/component'
+import React, { useState } from 'react'
 
-const Home = loadComponent(() => import('./components/home'))
-const List = loadComponent(() => import('./components/list'))
-const NotFound = loadComponent(() => import('./components/notFound'))
-const Detail = loadComponent(() => import('./components/detail'))
 
 /**
- * @loadable/component 
+ * 1 什么是 HOOK 
+ *  它就是一个特殊的函数 
+ *  它就可以让函数式组件也具有类组件的特性
+ * 
+ * 2 为什么需要 HOOK 
+ *  1 学习成本（相对）
+ *  2 数据共享
+ *  3 多个业务逻辑代码有可能会存在于同一个生命周期函数中
+ * 
+ * 3 useState 
+ *  它是一个可以在函数组件中保存状态的 HOOK 函数  
+ *  参数：保存状态的初始值 
+ *  返回值 
+ *    一个数组
+ *    第一个元素就是当前保存的状态
+ *    第二个元素就是修改当前保存状态的方法
  */
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <div>
-          <Link to="/home">首页</Link>---
-          <Link to="/list">列表页</Link>
-        </div>
-        <div>
-          <Switch>
-            <Route path="/" component={Home} exact />
-            <Route path="/home" component={Home} />
-            <AuthRouteGuard path="/list" component={List} />
-            <Route path="/detail/:id" component={Detail} />
-            <Route component={NotFound} />
-          </Switch>
-        </div>
-      </div>
-    )
-  }
-}
+function App() {
 
+  const arr = useState(0)
+  const [state, setState] = arr
+  console.log(state, setState)
+  return (
+    <div>
+      数据： {state}
+      <button onClick={() => { setState(state + 1) }}>+1</button>
+      <button onClick={() => { setState(state - 1) }}>-1</button>
+    </div>
+  )
+}
 export default App
 
