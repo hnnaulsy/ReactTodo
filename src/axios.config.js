9c4289db2dbe63be7abf1340ddab7eeb12d6c68c
hunaulsy <45566078+hnnaulsy@users.noreply.github.com>
2022-09-13 22:01:42 +0800

    实例代码优化

---
+++ src/axios.config.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+axios.defaults.baseURL = 'http://localhost:3005'
+
+axios.interceptors.response.use(res=>res.data)
--- src/store/saga/todo.saga.js
+++ src/store/saga/todo.saga.js
@@ -3,42 +3,42 @@ import {takeEvery,put, take} from 'redux-saga/effects'
 import { load_todo,load_todo_success,add_todo,add_todo_success,remove_todo,remove_todo_success,modify_todo,modify_todo_success,clear_todo_completed,clear_todo_completed_success,modify_todo_edit,modify_todo_edit_success,modify_todo_name,modify_todo_name_success } from '../actions/todo.actions'
 
 function* loade_todo_data(){
-  let todoData = yield axios.get('http://localhost:3005/api/todos').then(res=>res.data)
+  let todoData = yield axios.get('/api/todos')
   yield put(load_todo_success(todoData))
 }
 
 function* add_todo_data(action){
-  let taskInfo = yield axios.post('http://localhost:3005/api/todos',{taskName: action.payload}).then(res=>res.data)
+  let taskInfo = yield axios.post('/api/todos',{taskName: action.payload})
   yield put(add_todo_success(taskInfo.task))
 }
 
 function* remove_todo_data(action) {
-  let res = yield axios.delete('http://localhost:3005/api/todos',{
+  let res = yield axios.delete('/api/todos',{
     params: {
       id: action.payload
     }
-  }).then(res=>res.data)
+  })
   yield put(remove_todo_success(res.tasks.id))
 }
 
 function* modify_todo_data(action) {
   let params = action.payload
-  yield axios.put('http://localhost:3005/api/todos/isCompleted',params).then(res=>res.data)
+  yield axios.put('/api/todos/isCompleted',params)
   yield put(modify_todo_success(params))
 }
 
 function* clear_todo_data(action) {
-  yield axios.delete('http://localhost:3005/api/todos/clearCompleted')
+  yield axios.delete('/api/todos/clearCompleted')
   yield put(clear_todo_completed_success())
 }
 
 function* modify_todo_edit_data(action) {
-  yield axios.put('http://localhost:3005/api/todos/isEditing',action.payload)
+  yield axios.put('/api/todos/isEditing',action.payload)
   yield put(modify_todo_edit_success(action.payload))
 }
 
 function* modify_todo_name_data(action) {
-  yield axios.put('http://localhost:3005/api/todos/',action.payload)
+  yield axios.put('/api/todos/',action.payload)
   yield put(modify_todo_name_success(action.payload))
 }
 
